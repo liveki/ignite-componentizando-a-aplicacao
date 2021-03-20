d8d5e983ffcae9617872c445acaa6468b34f8496
Marlon Saldanha <marlonsaldanha@outlook.com>
2021-03-20 14:16:41 -0300

    feat(Components): adding new components into application

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import { Header } from './Header';
+import { GenreResponseProps, MovieProps } from '../App';
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <Header selectedGenre={selectedGenre} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.Title}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { GenreResponseProps } from '../App';
+
+import '../styles/header.scss';
+
+interface HeaderProps {
+    selectedGenre: GenreResponseProps;
+}
+
+export function Header({ selectedGenre }: HeaderProps) {
+    return (
+        <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+    )
+}
